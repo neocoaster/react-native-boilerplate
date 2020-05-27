fb4af116cb86e95e68e5eda55e8c6c235f48ea83
ldelarco <lucia@neocoast.com>
2020-05-27 17:05:08 -0300

    Nesting Navigators

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -13,3 +13,7 @@ export const HEADER_BACKGROUND = '#042152';
 // ERROR STYLES
 export const ERROR_TEXT = '#E60000';
 export const ERROR_BORDER = '#F00';
+
+// TAB NAVIGATOR STYLES
+export const ACTIVE_TAB = '#009933';
+export const INACTIVE_TAB = '#999999';
